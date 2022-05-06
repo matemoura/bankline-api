e4659757a613721979cbfbcef4451210d41c0109
Mateus Moura <mateus51moura@gmail.com>
2022-05-06 00:10:47 -0300

    Interação com front-end

--- src/main/java/com/moura/bankline/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/moura/bankline/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package com.moura.bankline.banklineapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.moura.bankline.banklineapi.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
 
+	public List<Movimentacao>findByIdConta(Integer idConta);	
 }
